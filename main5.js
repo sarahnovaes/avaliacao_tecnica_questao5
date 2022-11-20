8e15e7fbcaf400ca8b1097480718c414205fad4a
sarahnovaes <118676911+sarahnovaes@users.noreply.github.com>
2022-11-20 12:23:41 -0300

    Add files via upload

---
+++ main5.js
@@ -0,0 +1,13 @@
+var id = ['1','2','3','4','5','6','7']
+
+var nome = ['Fabiana Araújo', 'Gabriel Gomes', 'Fernando Henrique', 'Ana Luiza', 'Geralda do Nascimento', 'Miguel Souza', 'Antonio Miguel'];
+
+var idade = ['33', '25', '17', '2', '93', '70', '69'];
+
+console.log(id[0],nome[0], idade[0]);
+console.log(id[1],nome[1], idade[1]);
+console.log(id[2],nome[2], idade[2]);
+console.log(id[3],nome[3], idade[3]);
+console.log(id[4],nome[4], idade[4]);
+console.log(id[5],nome[5], idade[5]);
+console.log(id[6],nome[6], idade[6]);
